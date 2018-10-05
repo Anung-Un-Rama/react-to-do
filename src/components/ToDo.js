c80e4d455f578b571fdab54d663d4cd15990c3e7
Jared Smith <j.danielsmith451@gmail.com>
2018-10-05 12:02:04 -0700

    Submission for checkpoint7

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,10 +3,13 @@ import React, {Component} from 'react';
 class ToDo extends Component{
   render() {
     return (
-      <li> A todo will go here </li>
+      <li>{this.props.description} </li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
+      </li>
     );
   }
 }
 
 export default ToDo;
-//ToDo component for app
